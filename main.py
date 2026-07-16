daf045bc0438ac9a8a375d9fddac79a47d189ec3
JoshuaAndle <68359516+JoshuaAndle@users.noreply.github.com>
2026-07-16 04:50:32 -0400

    Added main and renamed llm_models module

---
+++ main.py
@@ -0,0 +1,102 @@
+
+# Requires transformers>=4.51.0
+
+import torch
+import torch.nn.functional as F
+
+from torch import Tensor
+from transformers import AutoTokenizer, AutoModel
+from llm_models import QwenInstruct, QwenReasoning
+
+
+def last_token_pool(last_hidden_states: Tensor,
+                 attention_mask: Tensor) -> Tensor:
+    left_padding = (attention_mask[:, -1].sum() == attention_mask.shape[0])
+    if left_padding:
+        return last_hidden_states[:, -1]
+    else:
+        sequence_lengths = attention_mask.sum(dim=1) - 1
+        batch_size = last_hidden_states.shape[0]
+        return last_hidden_states[torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths]
+
+
+def get_detailed_instruct(task_description: str, query: str) -> str:
+    return f'Instruct: {task_description}\nQuery:{query}'
+
+
+
+
+
+def main():
+
+    embedder_tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen3-Embedding-0.6B', padding_side='left')
+    embedder = AutoModel.from_pretrained('Qwen/Qwen3-Embedding-0.6B')
+
+
+
+    ### Note: the tokenizer pads up to the length of the longest input, up to at most the max_length value
+    max_length = 3192
+
+    # Tokenize the input texts
+    batch_dict_queries = embedder_tokenizer(
+        queries,
+        padding=True,
+        truncation=True,
+        max_length=max_length,
+        return_tensors="pt",
+    )
+    # Tokenize the input texts
+    batch_dict_documents = embedder_tokenizer(
+        documents,
+        padding=True,
+        truncation=True,
+        max_length=max_length,
+        return_tensors="pt",
+    )
+
+    batch_dict_queries.to(embedder.device)
+    batch_dict_documents.to(embedder.device)
+    with torch.no_grad():
+        start_time = time.time()
+        outputs = embedder(**batch_dict_queries)
+        query_embeddings = last_token_pool(outputs.last_hidden_state, batch_dict_queries['attention_mask'])
+        # normalize embeddings
+        query_embeddings = F.normalize(query_embeddings, p=2, dim=1)
+
+        print("Time taken to embed queries: ", time.time() - start_time)
+        start_time = time.time()
+
+        outputs = embedder(**batch_dict_documents)
+        document_embeddings = last_token_pool(outputs.last_hidden_state, batch_dict_documents['attention_mask'])
+        document_embeddings = F.normalize(document_embeddings, p=2, dim=1)
+        print("Time taken to embed documents: ", time.time() - start_time)
+
+        scores = (query_embeddings @ document_embeddings.T)
+
+    print(scores.tolist())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
